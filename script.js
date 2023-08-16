6c50035f378cda8d1f0d7f5a22876072d867f792
saravanan-22 <saravanan.skillsafari@gmail.com>
2023-08-16 09:43:05 +0530

    P3_Hamburgermenu

---
+++ script.js
@@ -0,0 +1,6 @@
+const button = document.querySelector("button");
+document.addEventListener("click", () => {
+  const menu = document.querySelector("ul");
+  // console.log(menu);
+  menu.classList.toggle("menu");
+});
